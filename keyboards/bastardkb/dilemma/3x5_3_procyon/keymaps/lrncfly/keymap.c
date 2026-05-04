7d7262fdca7b9e930eac1e6e6126dab911614109
Laurence Foley <lrncfly@users.noreply.github.com>
2026-05-04 13:30:45 +0000

    console enable

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/lrncfly/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/lrncfly/keymap.c
@@ -347,6 +347,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         backlight_decrease(); // Steps down by 1 (out of 16)
         return false;
     }
+    // uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int:
+    // "
+    //         "%u, count: %u\n",
+    //         keycode, record->event.key.col, record->event.key.row,
+    //         record->event.pressed, record->event.time,
+    //         record->tap.interrupted, record->tap.count);
     return true;
 };
 
