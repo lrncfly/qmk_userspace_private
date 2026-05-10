04c5419816aae81193defb91590247f0ef46a45a
Laurence Foley <lrncfly@users.noreply.github.com>
2026-05-10 15:34:15 +0000

    debug led and adding g_led_config

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/lrncfly/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/lrncfly/keymap.c
@@ -63,7 +63,7 @@ combo_t key_combos[] = {COMBO(combo4, KC_RBRC)};
 // clang-format off
 /** \brief QWERTY layout (3 rows, 10 columns). */
 #define LAYOUT_LAYER_BASE                                                                     \
-        KC_Q, KC_W, KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,          KC_I,   KC_O,    KC_P, \
+        KC_Q, KC_W, KC_E,    MY_DB_STEP,    MY_DB_TOGG,    KC_Y,    KC_U,          KC_I,   KC_O,    KC_P, \
         KC_A, KC_S, KC_D,    KC_F,    KC_G,    KC_H,    KC_J,          KC_K,   KC_L, KC_QUOT, \
         KC_Z, KC_X, KC_C,    KC_V,    KC_B,    KC_N,    KC_M,       KC_COMM, KC_DOT, KC_SLSH, \
                  ESC_MED, TAB_FUN, SPC_NAV, ENT_SYM, BSP_NUM, LGUI_T(KC_DEL)
@@ -263,62 +263,10 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 // clang-format on
 #endif // ENCODER_MAP_ENABLE
 
-// cool custom startup animation
-void matrix_init_user(void) { // Runs boot tasks for keyboard
-    rgblight_enable_noeeprom();
-    // rgblight_mode_noeeprom(RGB_MATRIX_BAND_VAL);
-    // rgblight_set_speed(200);
-    rgblight_sethsv(0, 255, 255);
-
-    // rgblight_enable();
-    // rgblight_sethsv(0,255,255);
-    // rgblight_mode(3);
-};
-
-#ifdef RGB_MATRIX_ENABLE
-// Layer state indicator
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-
-    uint8_t layer = get_highest_layer(layer_state);
-    if (layer > 0) {
-        HSV hsv = rgb_matrix_get_hsv();
-        switch (get_highest_layer(layer_state)) {
-        case 1:
-            hsv = (HSV){HSV_BLUE};
-            break;
-        case 2:
-            hsv = (HSV){HSV_AZURE};
-            break;
-        case 3:
-            hsv = (HSV){HSV_ORANGE};
-            break;
-        case 4:
-            hsv = (HSV){HSV_GREEN};
-            break;
-        case 5:
-            hsv = (HSV){HSV_TEAL};
-            break;
-        case 6:
-            hsv = (HSV){HSV_PURPLE};
-            break;
-        case 7:
-        default:
-            hsv = (HSV){HSV_RED};
-            break;
-        };
-
-        if (hsv.v > rgb_matrix_get_val()) {
-            hsv.v = MIN(rgb_matrix_get_val() + 22, 255);
-        }
-        RGB rgb = hsv_to_rgb(hsv);
-
-        for (uint8_t i = led_min; i < led_max; i++) {
-            rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-        }
-    }
-    return false;
-};
-#endif // RGB_MATRIX_ENABLE
+void keyboard_post_init_user(void) {
+    // This will show up in QMK Toolbox immediately on startup
+    uprintf("Keyboard is awake and debugging is ready.\n");
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
