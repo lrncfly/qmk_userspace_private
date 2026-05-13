151dc693c15b9a45814e2f19c94eab515cffbd49
Laurence Foley <lrncfly@users.noreply.github.com>
2026-05-13 21:29:22 +0000

    keymap sync

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/lrncfly/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/lrncfly/keymap.c
@@ -65,7 +65,7 @@ combo_t key_combos[] = {COMBO(combo4, KC_RBRC)};
 // clang-format off
 /** \brief QWERTY layout (3 rows, 10 columns). */
 #define LAYOUT_LAYER_BASE                                                                     \
-        KC_Q, KC_W, KC_E,    MY_DB_STEP,    MY_DB_TOGG,    KC_Y,    KC_U,          KC_I,   KC_O,    KC_P, \
+        KC_Q, KC_W, KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,          KC_I,   KC_O,    KC_P, \
         KC_A, KC_S, KC_D,    KC_F,    KC_G,    KC_H,    KC_J,          KC_K,   KC_L, KC_QUOT, \
         KC_Z, KC_X, KC_C,    KC_V,    KC_B,    KC_N,    KC_M,       KC_COMM, KC_DOT, KC_SLSH, \
                  ESC_MED, TAB_FUN, SPC_NAV, ENT_SYM, BSP_NUM, LGUI_T(KC_DEL)
